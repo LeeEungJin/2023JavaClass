0ea08f31f3e5d917d5669a5371eb84cac3c9d36e
dldmdwls45i@naver.com <dldmdwls45i@naver.com>
2023-11-21 14:57:41 +0900

    2023.11.21 14:55 Class Logic lesson

---
+++ JavaClassBasic/src/JavaClassBasic/JavaClassBasicFree.java
@@ -0,0 +1,9 @@
+package JavaClassBasic;
+
+public class JavaClassBasicFree {
+	public static void main(String[] args) {
+		System.out.println("hi");
+		
+	}
+
+}
---
+++ JavaPractice/src/JavePractice/Free.java
@@ -0,0 +1,15 @@
+package JavePractice;
+import java.util.Scanner;
+
+public class Free{
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Hi");
+		
+		
+		
+		
+		scanner.close();
+	}
+}

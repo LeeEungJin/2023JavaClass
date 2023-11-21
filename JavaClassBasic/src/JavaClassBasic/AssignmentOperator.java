accec7fbe073dbeecf58b7f470d97f62ab8986e5
dldmdwls45i@naver.com <dldmdwls45i@naver.com>
2023-11-21 15:11:19 +0900

    2023.11.21 15:10 Assignment Operator

---
+++ JavaClassBasic/src/JavaClassBasic/AssignmentOperator.java
@@ -0,0 +1,15 @@
+package JavaClassBasic;
+
+public class AssignmentOperator {
+	public static void main(String[] args) {
+		
+		int a=12;
+		int b=10;
+		
+		a += 10;
+		b -= 20;
+		
+		System.out.printf("%d\n%d", a, b);
+		
+	}
+}

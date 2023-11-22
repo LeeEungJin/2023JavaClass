f2dda344f2f0e2f9c312c640434a06e6f6b1c990
dldmdwls45i@naver.com <dldmdwls45i@naver.com>
2023-11-22 17:07:31 +0900

    2023.11.22 17:06 Java Basic Class, Times Table Add

--- JavaClassBasic/src/JavaClassBasic/TimesTable.java
+++ JavaClassBasic/src/JavaClassBasic/TimesTable.java
@@ -2,13 +2,17 @@ package JavaClassBasic;
 
 public class TimesTable {
 	public static void main(String[] args) {
-		int dan = 2;
-		int gop = 1;
+		
+		int dan = 0;
+		int gop = 0;
+		
 		String result = "";
 		
-		
-		for(gop=1 ; gop<10 ; gop++) {
-			result = result + ("2 * " + gop + " = " + (dan * gop) + "\n");
+		for (gop = 1 ; gop < 10 ; gop++) {
+			for (dan = 2 ; dan < 10 ; dan++ ) {
+				result = result + dan + "*" + gop + "=" + (dan*gop) +"\t";
+			}
+			result = result + "\n";
 		}
 		
 		System.out.println(result);
